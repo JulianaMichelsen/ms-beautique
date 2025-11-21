7c6175ad4cae338dcd4b15294495e065cf0725a3
Juliana Michelsen <jiic@gft.com>
2025-11-21 13:41:18 -0300

    feat: Criação das entities e configuração do banco PostgreSQL

---
+++ src/main/java/br/com/beautique/api/entities/CostumerEntity.java
@@ -0,0 +1,22 @@
+package br.com.beautique.api.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "customer")
+public class CostumerEntity extends BaseEntity{
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, length = 100)
+    private String phone;
+}
